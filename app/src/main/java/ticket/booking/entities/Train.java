e826d8764ff374ee2c57cf51e0b587b38a8495cf
NAVYDON2004 <rithikchandrasekar.22aim@kongu.edu>
2025-02-13 19:52:26 +0530

    getter and setter

--- app/src/main/java/ticket/booking/entities/Train.java
+++ app/src/main/java/ticket/booking/entities/Train.java
@@ -2,11 +2,52 @@ package ticket.booking.entities;
 import java.sql.Time;
 import java.util.*;
 public class Train {
-    private String trainName;
+    private String trainNo;
     private String trainId;
     private List<List<Integer>> seats;
-    private Map<String,Time> stationTimes;
-    private List<String> stations; 
+    private Map<String,String> stationTimes;
+    private List<String> stations;
+    public Train(String trainNo, String trainId, List<List<Integer>> seats, Map<String,String> stationTimes, List<String> stations){
+        this.trainNo = trainNo;
+        this.trainId = trainId;
+        this.seats = seats;
+        this.stationTimes = stationTimes;
+        this.stations=stations;
+    } 
+    public String gettrainNo(){
+        return trainNo;
+    }
+    public String gettrainId(){
+        return trainId;
+    }
+    public List<List<Integer>> getseats(){
+        return seats;
+    }
+    public Map<String,String> getstationTimes(){
+        return stationTimes;
+    }
+    public List<String> getstations(){
+        return stations;
+    }
+    public void settrainNo(String trainNo){
+        this.trainNo=trainNo;
+    }
+    public void settrainId(String trainId){
+        this.trainId=trainId;
+    }
+    public void setseats(List<List<Integer>> seats){
+        this.seats=seats;
+    }
+    public void setstationTimes(Map<String,String> stationTimes){
+        this.stationTimes=stationTimes;
+    }
+    public void setstations(List<String> stations){
+        this.stations=stations;
+    }
+    public String getTrainInfo(){
+        return String.format("TrainId:%s TrainNo:%s",trainId,trainNo)
+    }
+
 
 
 
--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -1,5 +1,6 @@
 package ticket.booking.entities;
 import java.util.*;
+import org.checkerframework.common.reflection.qual.GetMethod;
 
 public class User {
     private String name;
@@ -7,4 +8,48 @@ public class User {
     private String hashedPassword;
     private String userId;
     private List<Ticket> ticketsbooked;
+
+    public User(String name,String password, String hashedPassword,String UserId,List<Ticket> ticketsbooked){
+        this.name=name;
+        this.password=password;
+        this.hashedPassword=hashedPassword;
+        this.userId=UserId;
+        this.ticketsbooked=ticketsbooked;
+    }
+    public User(){}
+    public String getname(){
+        return name;
+    }
+    public String getpassword(){
+        return password;
+    }
+    public String gethashedPassword(){
+            return hashedPassword;
+    }
+    public String getuserId(){
+                return userId;
+    }
+    public List<Ticket> getticketsbooked(){
+        return ticketsbooked;
+    }
+    public void printTickets(){
+        for(int i=0;i<ticketsbooked.size();i++){
+            System.out.println(ticketsbooked.get(i).getTicketInfo());
+        }
+    }
+    public void setname(String name){
+        this.name=name;
+    }
+    public void setpassword(String password){
+        this.password=password;
+    }
+    public void sethashedPassword(String hashedPassword){
+        this.hashedPassword=hashedPassword;
+    }
+    public void setuserId(String userId){
+        this.userId=userId;
+    }
+    public void setticketsbooked(List<Ticket> ticketsbooked){
+        this.ticketsbooked=ticketsbooked;
+    }
 }

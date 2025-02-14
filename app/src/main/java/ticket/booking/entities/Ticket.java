f521a5a8072c4e163f1ac72ef781c05e22ea0951
NAVYDON2004 <rithikchandrasekar.22aim@kongu.edu>
2025-02-14 22:34:30 +0530

    hello

--- app/src/main/java/ticket/booking/entities/Ticket.java
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -8,4 +8,13 @@ public class Ticket {
     private String destination;
     private Date dateOfTravel;
     private Train train;
+
+    public Ticket(String ticketId, String userId, String source, String destination,Date dateOfTravel, Train train) {
+        this.ticketId = ticketId;
+        this.userId = userId;
+        this.source = source;
+        this.destination = destination;
+        this.dateOfTravel = dateOfTravel;
+        this.train = train;
+    }
 }
